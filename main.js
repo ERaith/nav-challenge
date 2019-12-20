b29523b22d644a1753d96a50d72bbd26b7b75aca
Erik Raith <raithhtiar@gmail.com>
2019-12-20 10:20:25 -0700

    Swap content out of main page based on navLink
    + Add a function with a switch statment based on what was clicked to swap content.
    + Removed HTML from index.html

---
+++ main.js
@@ -0,0 +1,45 @@
+var linksParent = document.querySelector('.container');
+linksParent.addEventListener('click', activeLink);
+
+function activeLink(event) {
+  if (event.target.classList.contains('navLink')) {
+    document.querySelector('.active').classList.remove('active');
+    event.target.classList.add('active');
+    swapContent(event);
+  }
+}
+
+function swapContent(event) {
+  var mainHTML = `<div class="all-courses">
+    <p>Share Udemy with Friends!</p>
+  </div>`;
+  var collectionsHTML = `<div class="collections">
+    <p>Organize</p>
+  </div>`;
+  var wishListHTML = `<div class="wishlist">
+    <button>Browse Stuff</button>
+  </div>`;
+  var archivedHTML = `  <div class="archived">
+      <h3>Past things</h3>
+      <p>Something something dark side</p>
+    </div>`;
+  var replacementHTML = '';
+  console.log(event.target.innerHTML);
+  switch (event.target.innerHTML) {
+    case 'All Courses':
+      replacementHTML = mainHTML;
+      break;
+    case 'Collections':
+      replacementHTML = collectionsHTML;
+      break;
+    case 'Wishlist':
+      replacementHTML = wishListHTML;
+      break;
+    case 'Archived':
+      replacementHTML = archivedHTML;
+      break;
+  }
+  var mainParent = document.querySelector('main');
+  mainParent.innerHTML = replacementHTML;
+
+}
