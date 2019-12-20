52fb2f9de9f76b17e34e2cd737a112b1a5588d78
ERaith <raithhtiar@gmail.com>
2019-12-20 09:53:30 -0700

    Revert "Feature/iteration2"

--- main.js
+++
@@ -1,9 +0,0 @@
-var linksParent = document.querySelector('.container');
-linksParent.addEventListener('click', activeLink);
-
-function activeLink(event){
-  if(event.target.classList.contains('navLink')){
-    document.querySelector('.active').classList.remove('active');
-    event.target.classList.add('active');
-  }
-}
